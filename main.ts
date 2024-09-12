7d97ba7f2a7b1105238d3b992e908f505a698cf8
ianyimi <anyiminene@gmail.com>
2024-09-12 00:06:29 -0700

    feat: display menu with vault taglist with @ symbol is pressed on the keyboard

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	type App,
+	type Editor,
+	getAllTags,
+	Menu,
+	MetadataCache,
+	MarkdownView,
+	Modal,
+	SuggestModal,
+	Notice,
+	Plugin,
+	Vault,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
@@ -7,51 +21,36 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	vaultTags: string[] | null;
+	vaultLinks: string[] | null;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+		this.vaultTags = this.getAllTags();
+		// this.vaultLinks = Vault.getMarkdownFiles().map(p => p.file.name)
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "sample-editor-command",
+			name: "Sample editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				editor.replaceSelection("Sample Editor Command");
+			},
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -62,7 +61,7 @@ export default class MyPlugin extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -70,16 +69,78 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
+		});
+
+		// Add event listener for keydown events
+		this.registerDomEvent(document, "keydown", (event: KeyboardEvent) => {
+			this.handleKeyPress(event);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
+		);
 	}
 
 	onunload() {
+		console.log("Autocomplete plugin unloaded");
+	}
 
+	getAllTags(): string[] {
+		const tagSet = new Set<string>();
+
+		// Get all files in the vault
+		const files = this.app.vault.getMarkdownFiles();
+
+		// Iterate through each file and get its cached metadata
+		for (const file of files) {
+			const cachedMetadata = this.app.metadataCache.getFileCache(file);
+
+			// Check if there are any tags in the metadata
+			if (cachedMetadata && cachedMetadata.tags) {
+				for (const tag of cachedMetadata.tags) {
+					tagSet.add(tag.tag); // Add each tag to the set
+				}
+			}
+		}
+
+		return Array.from(tagSet);
+	}
+
+	handleKeyPress(event: KeyboardEvent) {
+		if (event.key === "@") {
+			this.showSuggestionsAtCursor();
+		}
+	}
+
+	showSuggestionsAtCursor() {
+		const selection = window.getSelection();
+
+		if (selection && selection.rangeCount > 0) {
+			const range = selection.getRangeAt(0);
+			const rect = range.getBoundingClientRect(); // Get the cursor's bounding box
+
+			// Show the suggestion menu at the cursor position
+			this.showSuggestionMenu(rect.left, rect.bottom);
+		}
+	}
+
+	showSuggestionMenu(x: number, y: number) {
+		// Create a new suggestion menu using Obsidian's Menu API
+		const menu = new Menu();
+		console.log("vault tags: ", this.vaultTags);
+		for (const tag of this.vaultTags ?? []) {
+			menu.addItem((item) => {
+				item.setTitle(tag).onClick(() => {
+					console.log(`Selected tag #${tag}`);
+				});
+			});
+		}
+
+		// Display the menu at the cursor's position
+		menu.showAtPosition({ x: x, y: y });
 	}
 
 	async loadSettings() {
@@ -97,12 +158,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,19 +177,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
